8b069c7893328c390b4334aadf7b97b27cac0c81
Jesus <j.jimenez21@uniandes.edu.co>
2021-01-30 17:17:00 -0500

    Se añadió opción de cargar book_tags

--- App/view.py
+++ App/view.py
@@ -42,7 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TO-DO: Modificaciones para completar el laboratorio 1.
+    print("3- Cargar Tags de Libros")
     print("0- Salir")
 
 
@@ -59,6 +59,11 @@ def loadTags():
     """
     return controller.loadTags('GoodReads/tags.csv')
 
+def loadBooksTags():
+    """
+    Carga los Libros y Tags
+    """
+    return controller.loadBooksTags('GoodReads/book_tags-small.csv')
 
 """
 Menu principal
@@ -75,7 +80,10 @@ while True:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
-    # TO-DO: Modificaciones para completar el laboratorio 1.
+    elif int(inputs[0]) == 3:
+        print("Cargando información de tags y libros....")
+        book_tags = loadBooksTags()
+        print('Total de libros y tags cargados: ' + str(lt.size(book_tags)))
 
     else:
         sys.exit(0)
